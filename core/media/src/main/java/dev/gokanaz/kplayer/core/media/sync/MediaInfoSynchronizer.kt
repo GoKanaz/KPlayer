3b5ddd3e2703865188ec17e400cd05382ebf1d3a
GoKanaz <gokanaz.official@gmail.com>
2026-02-13 17:10:10 +0700

    Remove core/media folder

--- core/media/src/main/java/dev/gokanaz/kplayer/core/media/sync/MediaInfoSynchronizer.kt
+++
@@ -1,5 +0,0 @@
-package dev.gokanaz.kplayer.core.media.sync
-
-interface MediaInfoSynchronizer {
-    suspend fun syncInfo(mediaId: Long): Result<Unit>
-}
--- core/media/src/main/java/dev/gokanaz/kplayer/core/media/sync/MediaSynchronizer.kt
+++
@@ -1,5 +0,0 @@
-package dev.gokanaz.kplayer.core.media.sync
-
-interface MediaSynchronizer {
-    suspend fun sync(): Result<Unit>
-}
