0dfe08e5b1ae2e3592decf56a81dce64fb8e591e
GoKanaz <gokanaz.official@gmail.com>
2026-02-14 09:22:26 +0700

    Fix core:domain change to Android library plugin

--- core/domain/build.gradle.kts
+++ core/domain/build.gradle.kts
@@ -1,25 +1,32 @@
 plugins {
-    id("java-library")
-    id("org.jetbrains.kotlin.jvm")
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
     id("kotlin-kapt")
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-}
+android {
+    namespace = "dev.gokanaz.kplayer.core.domain"
+    compileSdk = 34
 
-kotlin {
-    jvmToolchain(17)
+    defaultConfig {
+        minSdk = 24
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
 dependencies {
     implementation(project(":core:media"))
-    
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-    
     testImplementation("junit:junit:4.13.2")
     testImplementation("io.mockk:mockk:1.13.8")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
-    testImplementation("org.jetbrains.kotlin:kotlin-test:1.9.20")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:1.9.22")
 }
