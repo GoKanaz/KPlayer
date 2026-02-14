195dd427d131587c0197b80c98a21b1280b855c4
GoKanaz <gokanaz.official@gmail.com>
2026-02-14 11:23:13 +0700

    Fix core:domain add hilt dependency

--- core/domain/build.gradle.kts
+++ core/domain/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("dagger.hilt.android.plugin")
     id("kotlin-kapt")
 }
 
@@ -24,6 +25,8 @@ android {
 
 dependencies {
     implementation(project(":core:media"))
+    implementation("com.google.dagger:hilt-android:2.48")
+    kapt("com.google.dagger:hilt-compiler:2.48")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     testImplementation("junit:junit:4.13.2")
     testImplementation("io.mockk:mockk:1.13.8")
