25ea707841e44c00db4f33d1a503a339731cc7ec
GoKanaz <gokanaz.official@gmail.com>
2026-02-15 23:46:30 +0700

    Fix: Remove Video dependency from ShareVideosUseCase

--- core/domain/src/main/java/dev/gokanaz/kplayer/core/domain/usecase/ShareVideosUseCase.kt
+++ core/domain/src/main/java/dev/gokanaz/kplayer/core/domain/usecase/ShareVideosUseCase.kt
@@ -1,10 +1,9 @@
 package dev.gokanaz.kplayer.core.domain.usecase
 
 import android.content.Context
-import dev.gokanaz.kplayer.core.model.Video
 import javax.inject.Inject
 
 class ShareVideosUseCase @Inject constructor() {
-    suspend operator fun invoke(videos: List<Video>, context: Context) {
+    suspend operator fun invoke(videos: List<Any>, context: Context) {
     }
 }
